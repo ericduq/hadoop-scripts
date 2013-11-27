5d3ba191b917248151b649bf760b9d80be3de700
ericduq <ericduq@gmail.com>
2013-11-27 20:36:16 +0000

    first commit

---
+++ make-single-node.sh
@@ -0,0 +1,71 @@
+cd ~ 
+
+# Download java jdk
+sudo apt-get install openjdk-7-jdk
+cd /usr/lib/jvm
+sudo ln -s java-7-openjdk-amd64 jdk
+
+# Uncommment to install ssh 
+sudo apt-get install openssh-server
+
+# Add hadoop user
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hduser
+sudo adduser hduser sudo
+
+# Generate keys
+sudo -u hduser ssh-keygen -t rsa -P ''
+sudo sh -c 'cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys'
+#ssh localhost
+
+# Download Hadoop and set permissons
+cd ~
+wget http://www.trieuvan.com/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz
+sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
+cd /usr/local
+sudo mv hadoop-2.2.0 hadoop
+sudo chown -R hduser:hadoop hadoop
+
+# Hadoop variables
+sudo sh -c 'echo export JAVA_HOME=/usr/lib/jvm/jdk/ >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_INSTALL=/usr/local/hadoop >> /home/hduser/.bashrc'
+sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/bin >> /home/hduser/.bashrc'
+sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/sbin >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_MAPRED_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_COMMON_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_HDFS_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export YARN_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+
+# Modify JAVA_HOME 
+cd /usr/local/hadoop/etc/hadoop
+sudo -u hduser sed -i.bak s=\${JAVA_HOME}=/usr/lib/jvm/jdk/=g hadoop-env.sh
+pwd
+
+# Check that Hadoop is installed
+hadoop version
+
+# Edit configuration files
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>fs\.default\.name\</name>\<value>hdfs://localhost:9000\</value>\</property>=g' core-site.xml 
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn\.nodemanager\.aux-services</name>\<value>mapreduce_shuffle</value>\</property>\<property>\<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\<value>org\.apache\.hadoop\.mapred\.ShuffleHandler</value>\</property>=g' yarn-site.xml
+  
+sudo -u hduser mv mapred-site.xml.template mapred-site.xml
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>mapreduce\.framework\.name</name>\<value>yarn</value>\</property>=g' mapred-site.xml
+ 
+cd ~
+sudo mkdir -p mydata/hdfs/namenode
+sudo mkdir -p mydata/hdfs/datanode
+
+cd /usr/local/hadoop/etc/hadoop
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>dfs\.replication</name>\<value>1\</value>\</property>\<property>\<name>dfs\.namenode\.name\.dir</name>\<value>file:/home/hduser/mydata/hdfs/namenode</value>\</property>\<property>\<name>dfs\.datanode\.data\.dir</name>\<value>file:/home/hduser/mydata/hdfs/datanode</value>\</property>=g' hdfs-site.xml
+
+
+# Format Namenode
+hdfs namenode -format
+
+# Start Hadoop Service
+#sudo -u hduser start-dfs.sh
+#sudo -u hduser start-yarn.sh
+
+# Check status
+#sudo -u hduser jps
+
